f6422b3678565800171fbad06f23f13e6ddbd421
catiminca <catiminca@gmail.com>
2025-01-20 18:44:37 +0200

    Add new Arduino code

--- src/main_arduino.cpp
+++ src/main_arduino.cpp
@@ -18,15 +18,15 @@ float sensorValue_mq135;
 int buzzerPin = 8;
 int ledPin_mq135 = 9;
 
-unsigned long previousMillis_temp = 0; // To track the time
+unsigned long previousMillis_temp = 0;
 
-const unsigned long interval = 15000; // 15 seconds interval in milliseconds
+const unsigned long interval = 10000;
 
 unsigned long currentMillis_temp;
 
 unsigned long currentMillis_mq135;
 unsigned long currentMillis_mq2;
-const unsigned long interval_mq135 = 7000;
+const unsigned long interval_sensors = 5000;
 unsigned long previousMillis_mq135 = 0;
 unsigned long previousMillis_mq2 = 0;
 
@@ -47,42 +47,37 @@ void setup() {
   dht.temperature().getSensor(&sensor);
   dht.humidity().getSensor(&sensor);
 
-  //photoresistor
   pinMode(ledPin_photoresistor,  OUTPUT);
   pinMode(ledPin_mq135, OUTPUT);
-  pinMode(A0, INPUT);
-  
-  Serial.println("MQ2 & MQ135 are warming up!");
-	delay(10000); // allow the MQ2 and MQ135 to warm up
-  Serial.println("Warming up done!");
-
   pinMode(buzzerPin, OUTPUT);
 
-  // tone(buzzerPin, 1000, 2000);
+  pinMode(A0, INPUT);
+
+  Serial.println("MQ2 & MQ135 are warming up!");
+	delay(10000);
+  Serial.println("Warming up done!");
+
 }
 
-  // Create JSON payload
+  // Construct payload
   void sendPayload(float temperature, float humidity, int photoresistor, float mq135, float mq2) {
-    // Creează un obiect JSON
     StaticJsonDocument<200> jsonDoc;
 
-    // Adaugă valori în obiectul JSON
     jsonDoc["temperature"] = temperature;
     jsonDoc["humidity"] = humidity;
     jsonDoc["photoresistor"] = photoresistor;
     jsonDoc["airQuality"] = mq135;
     jsonDoc["gas"] = mq2;
 
-    // Serializare JSON și trimitere către ESP
     serializeJson(jsonDoc, espSerial);
-    serializeJson(jsonDoc, Serial); // Pentru debugging
-    Serial.println(); // Linie nouă pentru claritate
+    serializeJson(jsonDoc, Serial);
+    Serial.println();
 }
 
 
 void loop() {
 
-  
+  //temperature and humidity
   currentMillis_temp = millis();
   currentMillis_mq135 = millis();
   currentMillis_mq2 = millis();
@@ -105,11 +100,11 @@ void loop() {
     digitalWrite(ledPin_photoresistor, LOW);
   }
     
+  //MQ135 sensor
   sensorValue_mq135 = analogRead(A1);
-  // currentMillis_mq135 = millis();
   if (sensorValue_mq135 > 400) {
     Serial.print(" | Air quality not good: ");
-    Serial.print(sensorValue_mq135, DEC); // prints the value read
+    Serial.print(sensorValue_mq135, DEC);
     Serial.println(" PPM");
     airQualityFlag = true;
     digitalWrite(ledPin_mq135, HIGH);
@@ -119,29 +114,30 @@ void loop() {
     digitalWrite(ledPin_mq135, LOW);
   }
   
-  sensorValue_mq2 = analogRead(A3); // read analog input pin 0
+  //MQ2 sensor
+  sensorValue_mq2 = analogRead(A3);
   if(sensorValue_mq2 > 500) {
     Serial.print(" | Smoke or gas detected! ");
-    // Serial.print("Sensor Value is : ");
     Serial.println(sensorValue_mq2, DEC);
     gasFlag = true;
-    tone(buzzerPin, 110); // A4
-    // delay(1000);
+    tone(buzzerPin, 110);
 
     delay(2000);
   } else {
-    noTone(buzzerPin);  // delay(1000);
+    noTone(buzzerPin);
     gasFlag = false;
   }
 
-  if (airQualityFlag == true && currentMillis_mq135 - previousMillis_mq135 >= interval_mq135) {
+  // Measurements past threshold send each 7 seconds
+  if (airQualityFlag == true && currentMillis_mq135 - previousMillis_mq135 >= interval_sensors) {
+    previousMillis_mq135 = currentMillis_mq135;
     sendPayload(event_t.temperature, event_h.relative_humidity, sensorValue, sensorValue_mq135, sensorValue_mq2);
-    // delay(10000);
-  } else if (gasFlag == true && currentMillis_mq2 - previousMillis_mq2 >= interval_mq135) {
+  } else if (gasFlag == true && currentMillis_mq2 - previousMillis_mq2 >= interval_sensors) {
+    previousMillis_mq2 = currentMillis_mq2;
     sendPayload(event_t.temperature, event_h.relative_humidity, sensorValue, sensorValue_mq135, sensorValue_mq2);
-    // delay(10000);
   }
 
+  // Measurements send each 15 seconds
   if (currentMillis_temp - previousMillis_temp >= interval && airQualityFlag == false && gasFlag == false) {
     previousMillis_temp = currentMillis_temp;
     sendPayload(event_t.temperature, event_h.relative_humidity, sensorValue, sensorValue_mq135, sensorValue_mq2);
